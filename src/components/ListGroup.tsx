a40df26f497b2cb0d62eab27b9ee3bc6e536585c
prabhu-raja <prabhuraja@gmail.com>
2023-04-22 21:07:15 -0400

    10 - Event onClick inline

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 function ListGroup() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'Toronto'];
-  items = [];
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(evt) => console.log(item, evt)}>
             {item}
           </li>
         ))}
