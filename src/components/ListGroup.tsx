278ea4dde7e0d9e51ef033bd01367ab9f4968446
prabhu-raja <prabhuraja@gmail.com>
2023-04-22 20:29:46 -0400

    7-😉Fragment shortcut

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 
 function ListGroup() {
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
@@ -11,7 +11,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-    </Fragment>
+    </>
   );
 }
 export default ListGroup;
