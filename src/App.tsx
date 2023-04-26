7a4ea74864affa0113a7317069c431757caabe2a
prabhu-raja <prabhuraja@gmail.com>
2023-04-25 22:04:54 -0400

    16 - clean and setup ListGroup and commented Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 import Alert from './components/Alert';
 import Button from './components/Button';
+import ListGroup from './components/ListGroup/ListGroup';
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  // const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ['New York', 'San Francisco', 'Tokyo', 'Toronto'];
+  const handleSelectItem = (item: string) => {
+    console.log('🙋‍♂️', item);
+  };
+
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Hello <span>World!</span>
-        </Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My button</Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}></ListGroup>
     </div>
+    // <div>
+    //   {alertVisible && (
+    //     <Alert onClose={() => setAlertVisibility(false)}>
+    //       Hello <span>World!</span>
+    //     </Alert>
+    //   )}
+    //   <Button onClick={() => setAlertVisibility(true)}>My button</Button>
+    // </div>
   );
 }
 
