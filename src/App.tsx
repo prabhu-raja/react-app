7926a3b62133790d7ff5580168d27252eaf6dfeb
prabhu-raja <prabhuraja@gmail.com>
2023-04-30 12:47:33 -0400

    24 - Update array of object by useState

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,35 @@ import './App.css';
 
 function App() {
   // const [alertVisible, setAlertVisibility] = useState(false);
-  const items = ['New York', 'San Francisco', 'Tokyo', 'Toronto'];
-  const handleSelectItem = (item: string) => {
-    console.log('🙋‍♂️', item);
+  // const items = ['New York', 'San Francisco', 'Tokyo', 'Toronto'];
+  // const handleSelectItem = (item: string) => {
+  //   console.log('🙋‍♂️', item);
+  // };
+  const [bugs, setBugs] = useState([
+    { id: 1, title: 'Bug1', isFixed: false },
+    { id: 2, title: 'Bug2', isFixed: false },
+  ]);
+
+  const handleClick = () => {
+    setBugs(
+      bugs.map((bug) => (bug.id === 1 ? { ...bug, isFixed: true } : bug))
+    );
   };
 
   return (
     <div>
-      <BsFillCalendarFill color="red" size="40" />
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}></ListGroup>
-      <Button>My button</Button>
-      <Like onClick={() => console.log('👍')}></Like>
+      <button onClick={handleClick}>Update bug🐞</button>
     </div>
+    // <div>
+    //   <BsFillCalendarFill color="red" size="40" />
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectItem}></ListGroup>
+    //   <Button>My button</Button>
+    //   <Like onClick={() => console.log('👍')}></Like>
+    // </div>
+    // ---
     // <div>
     //   {alertVisible && (
     //     <Alert onClose={() => setAlertVisibility(false)}>
