f63f75525678e019052590efa779bde5b59a3531
prabhu-raja <prabhuraja@gmail.com>
2023-04-30 15:45:53 -0400

    27 - sharing state between components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import ListGroup from './components/ListGroup';
 import Button from './components/Button/Button';
 import Like from './components/Like';
 import './App.css';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
 
 function App() {
   // const [alertVisible, setAlertVisibility] = useState(false);
@@ -13,6 +15,7 @@ function App() {
   // const handleSelectItem = (item: string) => {
   //   console.log('🙋‍♂️', item);
   // };
+  /*
   const [bugs, setBugs] = useState([
     { id: 1, title: 'Bug1', isFixed: false },
     { id: 2, title: 'Bug2', isFixed: false },
@@ -29,16 +32,27 @@ function App() {
       })
     );
   };
+  */
+
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
+
+  const handleClear = () => {
+    setCartItems([]);
+  };
 
   return (
-    <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {JSON.stringify(bug.isFixed)}
-        </p>
-      ))}
-      <button onClick={handleClick}>Update bug🐞</button>
-    </div>
+    <>
+      <NavBar cartItemsCount={cartItems.length}></NavBar>
+      <Cart cartItems={cartItems} onClear={handleClear}></Cart>
+    </>
+    // <div>
+    //   {bugs.map((bug) => (
+    //     <p key={bug.id}>
+    //       {bug.title} {JSON.stringify(bug.isFixed)}
+    //     </p>
+    //   ))}
+    //   <button onClick={handleClick}>Update bug🐞</button>
+    // </div>
     // <div>
     //   <BsFillCalendarFill color="red" size="40" />
     //   <ListGroup
