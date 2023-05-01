b8db644e7913a24186c3601b8546630a3c4d81b6
prabhu-raja <prabhuraja@gmail.com>
2023-05-01 13:59:57 -0400

    32 - 📈 - Accessing Input fields

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,9 +1,17 @@
-import { FormEvent } from 'react';
+import { FormEvent, useRef } from 'react';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: '', age: 0 };
+
   const handleSubmit = (evt: FormEvent) => {
     evt.preventDefault();
-    console.log('form submit');
+    person.name = nameRef.current?.value || '';
+    if (ageRef.current !== null) {
+      person.age = parseInt(ageRef.current.value);
+    }
+    console.log(person);
   };
 
   return (
@@ -12,13 +20,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
