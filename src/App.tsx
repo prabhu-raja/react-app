6acdbe5555c0a0161444bbefadd85084fcf4bca3
prabhu-raja <prabhuraja@gmail.com>
2023-05-02 16:52:48 -0400

    37 - 💰 - Expense Filter

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,27 @@ import Cart from './components/Cart';
 import ExpandableText from './components/ExpandableText';
 import Form from './Form';
 import ExpenseList from './expense-tracker/components/ExpenseList';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [expenses, setExpenses] = useState([
-    { id: 1, description: 'aaa', amount: 10, category: 'utilities' },
-    { id: 2, description: 'aaa', amount: 10, category: 'utilities' },
-    { id: 3, description: 'aaa', amount: 10, category: 'utilities' },
-    { id: 4, description: 'aaa', amount: 10, category: 'utilities' },
+    { id: 1, description: 'aaa', amount: 10, category: 'Groceries' },
+    { id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 10, category: 'Groceries' },
+    { id: 4, description: 'ddd', amount: 10, category: 'Entertainment' },
   ]);
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((exp) => exp.category === selectedCategory)
+    : expenses;
 
   return (
     <>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={setSelectedCategory}></ExpenseFilter>
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((exp) => exp.id !== id))}
       />
     </>
