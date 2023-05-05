118488e1231baa97cf9f6583b24ca8550f7f4153
prabhu-raja <prabhuraja@gmail.com>
2023-05-05 10:12:20 -0400

    53 - custom 🪝to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { CanceledError } from './services/api-client';
 import userService, { User } from './services/user-service';
+import useUsers from './hooks/useUsers';
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err?.message);
-        setLoading(false);
-      });
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const onDeleteUser = (usr: User) => {
     const originalUsers = [...users];
